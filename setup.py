bf9f5045d20997d8c6459f357936d7a7effc0cab
kurages <kuragebots@gmail.com>
2021-09-15 13:29:25 +0900

    うーーーーーーーーーん

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
 import setuptools
 
+__version__ = "1.2"
+
 with open("README.md", "r", encoding="utf-8") as f:
 	long_description = f.read()
 
 
 setuptools.setup(
-	name="chk_args",
-	version="1.1",
-	author="kurages",
-	author_email="kurages.dev@gmail.com",
-	description="アノテーションチェッカー",
-	long_description=long_description,
-	long_description_content_type="text/markdown",
-	url="https://github.com/kurages/python_annotations_checker",
-	project_urls={
+	name = "chk_args",
+	version = __version__,
+	author = "kurages",
+	author_email = "kurages.dev@gmail.com",
+	description = "アノテーションチェッカー",
+	long_description = long_description,
+	long_description_content_type = "text/markdown",
+	url = "https://github.com/kurages/python_annotations_checker",
+	project_urls = {
 		"Bug Tracker": "https://github.com/kurages/python_annotations_checker/issues",
 	},
-	classifiers=[
+	classifiers = [
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
 		"Operating System :: OS Independent",
 	],
-	package_dir={"": "src"},
-	packages=setuptools.find_packages(where="src"),
-	python_requires=">=3.6",
+	package_dir = {"": "src"},
+	packages = setuptools.find_packages(where="src"),
+	python_requires = ">=3.6",
 )
+
 
--- src/chk_args/chk_args.py
+++ src/chk_args/chk_args.py
@@ -1,36 +1,42 @@
 import inspect
 from typing import get_type_hints, Any
 
+
+class AnnotationMismatchError(TypeError):
+	def __init__(self, argName:str, annotationsType:Any):
+		self.message = f"Mismatch annotation and argument types.\nThe type of argument ({argName}) is {annotationsType}."
+
+	def __str__(self):
+		return f"AnnotationMismatchError: {self.message}"
+
+
 def chk_args(func):
 	spec = inspect.getfullargspec(func)
 	annotations = spec.annotations
 
-	def _chk_args(name, val):
-		if name in annotations:
-			if annotations[name] == Any:
+	def _chk_args(argName:str, val:Any):
+		if argName in annotations:
+			if annotations[argName] == Any:
 				return val
-			elif issubclass(type(val), annotations[name]):
+			elif issubclass(type(val), annotations[argName]):
 				#サブクラスのとき
 				return val
-			elif type(val) != annotations[name]:
-				if annotations[name] == float and type(val) == int:
-					#floatにintが渡された
-					return float(val)
-				elif annotations[name] == int and type(val) == float and val - int(val) == 0:
-					#intにn.0のfloatが渡された
-					return int(val)
-				raise TypeError(f"The type of argument ({name}) is {annotations[name]}.")
+			elif type(val) != annotations[argName]:
+				raise AnnotationMismatchError(argName, annotations[argName])
 			else:
 				return val
+		else:
+			#未指定
+			return val
 
-	def wrapper(*args, **kwargs):
+	def wrapper(*args:list, **kwargs:dict):
 		i = 0
 		_args = list(args)
-		for name, val in zip(spec.args, args):
-			_args[i] = _chk_args(name, val)
+		for argName, val in zip(spec.args, args):
+			_args[i] = _chk_args(argName, val)
 			i += 1
-		for key, val in zip(list(kwargs.keys()),list(kwargs.values())):
-			kwargs[key] = _chk_args(key, val)
+		for argName, val in zip(list(kwargs.keys()),list(kwargs.values())):
+			kwargs[key] = _chk_args(argName, val)
 		return func(*_args, **kwargs)
 	return wrapper
 
